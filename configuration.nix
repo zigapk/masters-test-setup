0f26d606e93b6961df9a15f40d5dc17305c01339
Žiga Patačko Koderman <ziga@zerodays.dev>
2026-03-01 10:54:24 +0100

    fix: typo

--- configuration.nix
+++ configuration.nix
@@ -138,7 +138,7 @@
       "realtime"
     ];
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 aaaac3nzac1lzdi1nte5aaaaig5r1mt9pllx7ca8f6zvzskrp/k9spvsrsbensnyumry"
+      "ssh-ed25519 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG5r1Mt9pLlX7cA8F6ZVZSkrP/k9sPVSrSbeNSnyumrY"
     ];
   };
 
